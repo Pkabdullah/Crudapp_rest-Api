183d29709c98dfb562b67ffdc84300006dbfeb78
Muhammad Abdullah <124899260+Pkabdullah@users.noreply.github.com>
2024-02-06 16:20:02 +0500

    Update EditForm.tsx

--- src/components/EditForm.tsx
+++ src/components/EditForm.tsx
@@ -1,72 +1,67 @@
-  "use client"
-  import { Button } from '@/components/ui/button'
-  import { Input } from '@/components/ui/input'
-  import { useRouter } from 'next/navigation'
-  import React, { useState } from 'react'
+"use client"
+import { useState } from 'react'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+import { useRouter } from 'next/navigation'
+
+const EditForm = ({ id, name, quantity, amount }: any) => {
+  const [newName, setNewName] = useState(name)
+  const [newAmount, setNewAmount] = useState(amount)
+  const [newQuantity, setNewQuantity] = useState(quantity)
+
+  const router = useRouter()
   
-  const AddItems = () => {
-    const [name, setname] = useState("")
-    const [amount, setamount] = useState("")
-    const [quantity, setquantity] = useState("")
-  
-    const router = useRouter()
-  
-    const handlesubmit = async (e: any) => {
-      e.preventDefault();
-  
-      if (!name || !amount || !quantity) {
-        alert("Item, Amount and Quantity are required.")
-        return;
+  const handleSubmit = async (e: any) => {
+    e.preventDefault()
+    try {
+      const res = await fetch(`http://localhost:3000/api/List/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ newName, newAmount, newQuantity })
+      })
+      if (!res.ok) {
+        throw new Error("Failed To Update new Items ")
       }
-  
-      try {
-        const res = await fetch("http://localhost:3000/api/List", {
-          method: "POST",
-          body: JSON.stringify({ name, amount, quantity }),
-        });
-        if (res.ok) {
-          router.push("/")
-          router.refresh()
-        } else {
-          throw Error("Failed to add Item")
-        }
-      } catch (error) {
-        console.log(error)
-      }
+
+      router.push("/")
+      router.refresh();
+    } catch (error) {
+      console.log(error)
     }
-  
-    return (
-      <form onSubmit={handlesubmit} className="p-4  ">
-        <div className=' grid grid-cols-1 md:grid-cols-3 gap-5 items-start'>
-          <div>
-            <h3 className='font-bold text-lg'>Items</h3>
-            <Input
-              placeholder='Items Name'
-              onChange={(e) => setname(e.target.value)}
-              value={name}
-              className='w-full my-4 rounded bg-gray-200' />
-          </div>
-          <div>
-            <h3 className='font-bold text-lg'>Amount</h3>
-            <Input
-              placeholder='Amount'
-              onChange={(e) => setamount(e.target.value)}
-              value={amount}
-              className='w-full my-4 rounded bg-gray-200' />
-          </div>
-          <div>
-            <h3 className='font-bold text-lg'>Quantity</h3>
-            <Input
-              placeholder='Quantity'
-              onChange={(e) => setquantity(e.target.value)}
-              value={quantity}
-              className='w-full my-4 rounded bg-gray-200' />
-          </div>
-        </div>
-        <Button type='submit' className="mt-10 w-full md:w-auto bg-green-700 text-white hover:bg-gray-400">Update Items</Button>
-      </form>
-    )
   }
-  
-  export default AddItems
-  
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center">
+      <div className='p-4 border-2 border-slate-400 my-3 flex flex-col gap-5 items-start w-full max-w-md'>
+        <h3 className='font-sans font-bold text-lg'>Edit Items</h3>
+        <Input
+          placeholder='Items Name'
+          onChange={(e) => setNewName(e.target.value)}
+          value={newName}
+          className='my-4 rounded-none bg-gray-200 w-full'
+        />
+
+        <h3 className='font-sans font-bold text-lg'>Edit Amount</h3>
+        <Input
+          placeholder='Amount'
+          onChange={(e) => setNewAmount(e.target.value)}
+          value={newAmount}
+          className='my-4 rounded-none bg-gray-200 w-full'
+        />
+
+        <h3 className='font-sans font-bold text-lg'>Edit Quantity</h3>
+        <Input
+          placeholder='Quantity'
+          onChange={(e) => setNewQuantity(e.target.value)}
+          value={newQuantity}
+          className='my-4 rounded-none bg-gray-200 w-full'
+        />
+      </div>
+      <Button className="mt-10 bg-green-800 text-white hover:bg-gray-400">Update Items</Button>
+    </form>
+  )
+}
+
+export default EditForm
